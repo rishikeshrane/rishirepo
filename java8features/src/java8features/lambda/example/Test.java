094cea7120e2fb8179804498215daf1e5d1fd1e5
rishikeshrane <rishikeshrane@gmail.com>
2022-01-13 17:34:43 +0530

    lambda function example

---
+++ java8features/src/java8features/lambda/example/Test.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package java8features.lambda.example;
+
+import java.util.function.Function;
+
+/**
+ * @author Rishikesh
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Function<String, String> toUpper = (String str) -> str.toUpperCase();
+		String upperCase = toUpper.apply("Hello Lambda.");
+		System.out.println("UpperCase Result :" + upperCase);
+	}
+
+}
