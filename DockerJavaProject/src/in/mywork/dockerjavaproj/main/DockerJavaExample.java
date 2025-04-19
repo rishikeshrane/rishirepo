5518ebfe485ced9b8286d8820a1376abbbe6f370
rishikeshrane <rishikeshrane@gmail.com>
2025-04-19 06:59:16 +0530

    docker project example

---
+++ DockerJavaProject/src/in/mywork/dockerjavaproj/main/DockerJavaExample.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package in.mywork.dockerjavaproj.main;
+
+/**
+ * @author Rishikesh
+ *
+ */
+public class DockerJavaExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello, this is from docker platform.");
+	}
+
+}
